d5deade5a9ccaffe8ae598cbc218c25b32a254f3
주강 <jkpark@nkia.co.kr>
2022-12-13 16:10:08 +0900

    주석 추가

--- src/atom.js
+++ src/atom.js
@@ -18,6 +18,14 @@ export const globalLoggedInState = atom({
   default: false,
 });
 
+// localStorage 에 "token" 이라는 키값으로 만약 token 이 존재한다고 한다면
+// api 통신을 해서 해당 token 이 유효한지 판단하고, 로그인이 되었는지를 판별한다.
+// 이 작업은 layOut 이 렌더링 될 때 한번 처리됨.
+// 그리고 만약 localStorage 에 "token" 이라는 키값이 존재 하지 않는다면,
+// 굳이 서버와 통신할 필요가 없음. 이미 로그아웃 된 유저라는 의미,
+// 따라서 전역으로 관리되는 currentUserInfoQuery 라는 state 의 default 값으로
+// {result: false} 를 넣어주삼
+
 export const currentUserInfoQuery = selector({
   key: "CurrentUserInfoQuery",
   get: async () => {
